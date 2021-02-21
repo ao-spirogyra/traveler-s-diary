96158b1516711caa8d491e28867a9d66f3bd4ca2
ao-spirogyra <lily.spirogyra@gmail.com>
2021-02-21 13:11:06 +0900

    うまく動かないのでpuppeteerの周りでtry catchしない

--- src/function.ts
+++ src/function.ts
@@ -4,7 +4,6 @@ import fetch from 'node-fetch';
 import fs from 'fs';
 
 export const takeSnapshots = async (url,accessToken) => {
-  try {
     const browser = await puppeteer.launch({headless: true, args: ['--no-sandbox', '--disable​-setuid-sandbox']});
     const page = await browser.newPage();
     await page.setDefaultNavigationTimeout(0);
@@ -12,17 +11,11 @@ export const takeSnapshots = async (url,accessToken) => {
       width: 1920,
       height: 1080
     });
-    try {
-      await page.goto(url);
-      await page.screenshot({path: 'puppeteer/tmp.png', fullPage:true});
-    } catch (error) {
-      throw new Error(error)
-    } finally {
-      await browser.close();
-    }
-  } catch (error) {
-    throw new Error(error)
-  }
+    await page.goto(url);
+    await page.screenshot({path: 'puppeteer/tmp.png', fullPage:true});
+    await browser.close();
+
+
 
   const formData = new FormData();
   formData.append('access_token', accessToken);
