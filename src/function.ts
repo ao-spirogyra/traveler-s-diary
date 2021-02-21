ed23970ab8cd5912ae7398111fd03c109f9eb56a
ao-spirogyra <lily.spirogyra@gmail.com>
2021-02-21 13:23:39 +0900

    disableなんとかオプションを消す

--- src/function.ts
+++ src/function.ts
@@ -4,18 +4,16 @@ import fetch from 'node-fetch';
 import fs from 'fs';
 
 export const takeSnapshots = async (url,accessToken) => {
-    const browser = await puppeteer.launch({headless: true, args: ['--no-sandbox', '--disable​-setuid-sandbox']});
-    const page = await browser.newPage();
-    await page.setDefaultNavigationTimeout(0);
-    await page.setViewport({
-      width: 1920,
-      height: 1080
-    });
-    await page.goto(url);
-    await page.screenshot({path: 'puppeteer/tmp.png', fullPage:true});
-    await browser.close();
-
-
+  const browser = await puppeteer.launch({headless: true, args: ['--no-sandbox']});
+  const page = await browser.newPage();
+  await page.setDefaultNavigationTimeout(0);
+  await page.setViewport({
+    width: 1920,
+    height: 1080
+  });
+  await page.goto(url);
+  await page.screenshot({path: 'puppeteer/tmp.png', fullPage:true});
+  await browser.close();
 
   const formData = new FormData();
   formData.append('access_token', accessToken);
